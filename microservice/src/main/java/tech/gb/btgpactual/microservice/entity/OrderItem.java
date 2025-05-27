3fc3128c4891ce0ab9080068fda929f487a68767
biel.barros <gabrielbarrosg11@gmail.com>
2025-05-27 16:16:24 -0300

    feat: Mapeando entidades

---
+++ microservice/src/main/java/tech/gb/btgpactual/microservice/entity/OrderItem.java
@@ -0,0 +1,50 @@
+package tech.gb.btgpactual.microservice.entity;
+
+import java.math.BigDecimal;
+
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType; 
+
+public class OrderItem {
+
+    private String productId;
+    private Integer quantity;
+
+    @Field(targetType = FieldType.DECIMAL128)
+    private BigDecimal price;
+    
+    public OrderItem(String productId, Integer quantity, BigDecimal price) {
+        this.productId = productId;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+
+
+
+    
+}
