e3a897391d6f98b0401fdd0fec97b9cc22b34966
biel.barros <gabrielbarrosg11@gmail.com>
2025-06-04 13:28:24 -0300

    feat: add classe de resposta padrão da API

---
+++ microservice/src/main/java/tech/gb/btgpactual/microservice/controller/dto/ApiResponse.java
@@ -0,0 +1,10 @@
+package tech.gb.btgpactual.microservice.controller.dto;
+
+import java.util.List;
+import java.util.Map;
+
+public record ApiResponse<T>(Map<String, Object> summary,
+                             List<T> data, 
+                             PaginationResponse pagination) {
+
+}
