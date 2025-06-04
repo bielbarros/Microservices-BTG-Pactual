9034cab005b7e2da97def258206886c3363b0c07
biel.barros <gabrielbarrosg11@gmail.com>
2025-06-04 13:29:00 -0300

    refactor: melhorias e novas regras de negócio no serviço de pedidos

--- microservice/src/main/java/tech/gb/btgpactual/microservice/service/OrderService.java
+++ microservice/src/main/java/tech/gb/btgpactual/microservice/service/OrderService.java
@@ -6,15 +6,33 @@ import tech.gb.btgpactual.microservice.repository.OrderRepository;
 import tech.gb.btgpactual.microservice.dto.OrderCreatedEvent;
 import tech.gb.btgpactual.microservice.entity.OrderItem;
 import java.util.List;
+import org.springframework.data.domain.Page;
 import java.math.BigDecimal;
+import tech.gb.btgpactual.microservice.controller.dto.OrderResponse;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.MatchOperation;
+import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
+import org.springframework.data.mongodb.core.aggregation.UnwindOperation;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.core.aggregation.TypedAggregation;
+import org.bson.Document;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+
 
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final MongoTemplate mongoTemplate;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, MongoTemplate mongoTemplate) {
         this.orderRepository = orderRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public void save(OrderCreatedEvent event) {
@@ -27,6 +45,26 @@ public class OrderService {
         
         orderRepository.save(entity);
     }
+
+
+    public Page<OrderResponse> findAllByCustomerId(Long customerId, PageRequest pageRequest) {
+        var orders = orderRepository.findAllByCustomerId(customerId, pageRequest);
+
+        return orders.map(OrderResponse::fromEntity);
+
+    }
+
+    public BigDecimal findTotalOnOrdersByCostumerId(Long customerId) {
+        var aggregations = newAggregation(
+            match(Criteria.where("customerId").is(customerId)),
+            group("customerId").sum("total").as("total")    
+        );
+
+        var response = mongoTemplate.aggregate(aggregations, "tb_orders", Document.class);
+        
+        return new BigDecimal(response.getUniqueMappedResult().get("total").toString());
+    }
+
     
     private BigDecimal getTotal(OrderCreatedEvent event) {
         return event.itens().stream()
